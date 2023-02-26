4330ceb79d7e3308523b94fabf6b30824637fb58
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-26 02:57:45 +0100

    Test HardCoded file to test if /tmp folder is created

--- index.js
+++ index.js
@@ -9,24 +9,25 @@ dotenv.config();
 app.use(cors())
 var port = 5000;
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './tmp')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-})
+// var storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, './tmp')
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, file.originalname)
+//     }
+// })
 
-var upload = multer({ storage: storage })
+// var upload = multer({ storage: storage })
 
-app.use(express.static(__dirname + '/tmp'));
-app.use('./tmp', express.static('tmp'));
+// app.use(express.static(__dirname + '/tmp'));
+// app.use('./tmp', express.static('tmp'));
 
-app.post('/api/upload', upload.single('image'), async function (req, res, next) {
+// app.post('/api/upload', upload.single('image'), async function (req, res, next) {
+app.post('/api/upload', async function (req, res, next) {
     const options = {
         apiKey: process.env.IMGBB_KEY,
-        imagePath: "./tmp/"+req.file.filename
+        imagePath: "./tmp/inglis.png"
     }
     imgbbUploader(options)
         .then((response) => res.status(200).json(response))
